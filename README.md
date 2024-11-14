38d7c5a336ac014e565a7d5a71aea0f7a549db3f
as <allensnape@qq.com>
2024-11-14 21:08:40 +0800

    feat: list deps

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# __projectname__
+# __ProjectName__
 
 ## Dev
 
---
+++ scripts/list-deps.ts
@@ -0,0 +1,7 @@
+import Package from "../package.json";
+
+console.log(
+  "npm update",
+  Object.keys(Package.dependencies).join(" "),
+  Object.keys(Package.devDependencies).join(" "),
+);
