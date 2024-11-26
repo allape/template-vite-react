82f0b0036459dbcba5a70c0c10fb9d740087e955
as <allensnape@gmail.com>
2024-11-26 15:03:40 +0800

    refactor: refine script

--- scripts/replace-project-name.ts
+++ scripts/replace-project-name.ts
@@ -14,15 +14,15 @@ if (!/^([a-z0-9]-?)+$/.test(ProjectName)) {
   process.exit(1);
 }
 
-const CamelCaseProjectName = ProjectName.replace(/(([a-z])[^-]*-|-([a-z]))/g, (_, letter) => letter.toUpperCase());
+const CamelCaseProjectName = ProjectName.split("-").map((part) => part[0].toUpperCase() + part.slice(1)).join("");
 
 function replaceInFile(filePath: string, from: string, to: string) {
   const content = fs.readFileSync(filePath, "utf-8");
   fs.writeFileSync(filePath, content.replaceAll(from, to));
 }
 
-replaceInFile("../package.json", "__projectname__", ProjectName);
-replaceInFile("../package-lock.json", "__projectname__", ProjectName);
+replaceInFile("package.json", "__projectname__", ProjectName);
+replaceInFile("package-lock.json", "__projectname__", ProjectName);
 
-replaceInFile("../README.md", "__ProjectName__", CamelCaseProjectName);
-replaceInFile("../index.html", "__ProjectName__", CamelCaseProjectName);
+replaceInFile("README.md", "__ProjectName__", CamelCaseProjectName);
+replaceInFile("index.html", "__ProjectName__", CamelCaseProjectName);
