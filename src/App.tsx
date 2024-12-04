3f0a332a550cfe98233ae720736ac48e3b125dcd
as <allensnape@gmail.com>
2024-12-04 20:37:47 +0800

    refactor: refine

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,6 @@
-import viteLogo from "/vite.svg";
-import cls from "classnames";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { ReactElement } from "react";
 import styles from "./style.module.scss";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className={cls(styles.logo, styles.react)}
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={styles.card}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className={styles.readTheDocs}>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+export default function App(): ReactElement {
+  return <div className={styles.wrapper}>world</div>;
 }
-
-export default App;
